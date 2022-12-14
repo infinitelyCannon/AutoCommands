872f28f530d8ec8f741fc598a8d4ddf7084e17f4
Dakarai Simmons <infinitelyCannon@gmail.com>
2022-12-13 21:31:56 -0500

    Added need module for version 4.27

--- Source/AutoCommands/AutoCommands.Build.cs
+++ Source/AutoCommands/AutoCommands.Build.cs
@@ -7,29 +7,13 @@ public class AutoCommands : ModuleRules
 	public AutoCommands(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
+				"Core"
 			}
 			);
-			
 		
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
@@ -38,15 +22,7 @@ public class AutoCommands : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
+				"DeveloperSettings"
 			}
 			);
 	}
